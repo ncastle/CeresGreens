c0cf0b8426ce1d67c931737419795ac606a0a253
Nickolas.Castle <ncastle@uvm.edu>
2019-08-08 10:19:41 -0400

    grid werk

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>Ceres Green Application</h1>
+        <div id="dash">
+          <div className="box1">Box 1</div>
+          <div className="box2">Box 2</div>
+          <div className="box3">Box 3</div>
+          <div className="box4">Box 4</div>
+          <div className="box5">Box 5</div>
+        </div>
     </div>
   );
 }
