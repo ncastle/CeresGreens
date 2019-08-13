c30cfdaeea268506ad52f899e94574d3ecde1e24
mrshappy0 <ajs11300@gmail.com>
2019-08-13 11:47:22 -0400

    fetching data from open motics api

---
+++ proxy_server.js
@@ -0,0 +1,16 @@
+// backend proxy server for ceres app that routes a 
+// request from the client to openmotics
+const express = require('express');
+const proxy = require('express-http-proxy');
+const morgan = require('morgan');
+const config = require('./src/config');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(express.static('build'));
+app.use('/proxy', proxy(config.baseurl));
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
