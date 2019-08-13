20d761b49bb17aa2670695c6e6849d5633c2fb21
mrshappy0 <ajs11300@gmail.com>
2019-08-13 17:13:15 -0400

    style update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-const username = require('./secrets.json').username;
-const password = require('./secrets.json').password;
+
+// const username = require('./secrets.json').username;
+// const password = require('./secrets.json').password;
 
 
 class App extends React.Component {
@@ -11,9 +12,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       humidity: null,
+      light: null, // boolean -- "on" or "off"
+      waterTemp: null,
+      waterPump: null, // boolean -- "on" or "off"
+
+
       credentials: {
-        un: username, // username
-        pw: password  // password
+        // un: username, // username
+        // pw: password  // password
       }
     }
     this.login = this.login.bind(this);
@@ -21,7 +27,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    
+
     // this.login1();
     // fetch('https://cloud.openmotics.com/api/v1/base/installations/215/sensors/9', {
     //   method: 'GET',
@@ -52,60 +58,69 @@ class App extends React.Component {
       },
       mode: 'no-cors'
     })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
   }
 
   login() {
     var http = require("https");
 
-var options = {
-  "method": "POST",
-  "hostname": "cloud.openmotics.com",
-  "path": "/api/v1/authentication/basic/login",
-  "headers": {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Authorization": "Basic Og==",
-    "Cache-Control": "no-cache",
-    "Host": "cloud.openmotics.com",
-    "Accept-Encoding": "gzip, deflate",
-    "Content-Length": "69",
-    "Connection": "keep-alive",
-    "cache-control": "no-cache"
-  }
-};
+    var options = {
+      "method": "POST",
+      "hostname": "cloud.openmotics.com",
+      "path": "/api/v1/authentication/basic/login",
+      "headers": {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Basic Og==",
+        "Cache-Control": "no-cache",
+        "Host": "cloud.openmotics.com",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Length": "69",
+        "Connection": "keep-alive",
+        "cache-control": "no-cache"
+      }
+    };
 
-var req = http.request(options, function (res) {
-  var chunks = [];
+    var req = http.request(options, function (res) {
+      var chunks = [];
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
 
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-});
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
 
-req.write(JSON.stringify({ username: this.state.credentials.un, password: this.state.credentials.pw }));
-req.end();
+    req.write(JSON.stringify({ username: this.state.credentials.un, password: this.state.credentials.pw }));
+    req.end();
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Ceres Green Application</h1>
-          <div id="dash">
-            <div id="box1">Humidity: {this.state.humidity}</div>
-            <div id="box2">Box 2</div>
-            <div id="box3">Box 3</div>
-            <div id="box4">Box 4</div>
-            <div id="box5">Box 5</div>
+        <h1>Ceres Green</h1>
+        <h2>Basildash</h2>
+        <div id="dash">
+          <div id="topNav">
+            <ul id="navItems">
+              <li>Air</li>
+              <li>Water</li>
+              <li>Light</li>
+            </ul>
           </div>
+          <div id="h">Humidity: {this.state.humidity}</div>
+          <div id="w">Water Temp and Level:</div>
+          <div id="l">Light:</div>
+          <div id="c">Box c</div>
+          <div id="a">Box a</div>
+          <div id="p">Box p</div>
+        </div>
       </div>
     );
   }
