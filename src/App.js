0a5f598c35eb0b39f7cc3d2c54aaa43c189ba80d
Nickolas.Castle <ncastle@uvm.edu>
2019-08-14 13:13:48 -0400

    getting average air temperature and humidity from influxdb

--- src/App.js
+++ src/App.js
@@ -4,48 +4,145 @@ import './App.css';
 
 const config = require('./config.js');
 const fs = require('fs');
+// require influx for use in app
+const Influx = require('influx');
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sensor1: {
-        id: 0,
+      installationId: config.installationId,
+      airAvgs: {
         humidity: null,
         temperature: null
-      }
+      },
+      sensors: [
+        {
+          id: 0,
+          name: "T/H Sensor 1",
+          humidity: null,
+          temperature: null
+        },
+        {
+          id: 1,
+          name: "T/H Sensor 2",
+          humidity: null,
+          temperature: null
+        },
+        {
+          id: 2,
+          name: "T/H Sensor 3",
+          humidity: null,
+          temperature: null
+        },
+      ],
+      outputs: [
+        {
+          id: 5,
+          name: "A1 Pump",
+          on: undefined
+        },
+        {
+          id: 6,
+          name: "A2 Pump",
+          on: undefined
+        },
+        {
+          id: 7,
+          name: "A3 Pump",
+          on: undefined
+        },
+      ]
     }
+    this.openmoticsLogin = this.openmoticsLogin.bind(this);
+    this.getOMSensorInfo = this.getOMSensorInfo.bind(this);
   }
 
   async componentDidMount() {
     console.log({config});
     console.log(config.password);
     let token = null;
-    // login using proxy, username and pass
-    await fetch('/proxy/api/v1/authentication/basic/login', {
+    /*** OpenMotics ***/
+
+    token = await this.openmoticsLogin(config.username, config.password);
+
+    console.log('this is the token: ', token);
+
+    await this.getOMSensorInfo(token, 0);
+
+    // fetch outputs
+    await fetch('/proxy/api/v1/base/installations/215/outputs', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+    });
+
+    /*** InfluxDB ***/
+
+    // url to database: https://gigawatt-dbd9c7a7.influxcloud.net:8086
+
+    // testing querying the influx database
+    const influxdb =  new Influx.InfluxDB(`https://${config.dbusername}:${config.dbpassword}@gigawatt-dbd9c7a7.influxcloud.net:8086/openmotics`);
+    
+    console.log('connected to database?');
+    influxdb.getDatabaseNames()
+    .then(names => console.log(names));
+
+    // query influx database for current mean temperature
+    influxdb.query(`SELECT mean("temp") AS "mean_temp" FROM "openmotics"."autogen"."sensor" WHERE ("id"='0' OR "id"='1' OR "id"='2')`)
+    .then(results => {
+      console.log(results[0]);
+      const { airAvgs } = this.state;
+      airAvgs.temperature = results[0].mean_temp;
+      this.setState({airAvgs});
+    })
+
+    //query for current mean humidity
+    influxdb.query(`SELECT mean("hum") AS "mean_hum" FROM "openmotics"."autogen"."sensor" WHERE ("id"='0' OR "id"='1' OR "id"='2')`)
+    .then(results => {
+      console.log(results[0]);
+      const { airAvgs } = this.state;
+      airAvgs.humidity = results[0].mean_hum;
+      this.setState({airAvgs});
+    })
+
+  }
+
+  // function takes a username and password for openmotics, logs in,
+  // and returns a token that can be used to make other requests to openmotics
+  async openmoticsLogin(username, password) {
+    // login to openmotics using proxy, username and pass
+    return await fetch('/proxy/api/v1/authentication/basic/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
       body: JSON.stringify({
-        'username': config.username,
-        'password': config.password
+        'username': username,
+        'password': password
       })
     })
     .then(response => response.json())
     .then(json => {
       console.log(json);
       console.log(json.data.token);
-      token = json.data.token;      // assign token to token variable
+      return json.data.token;      // assign token to token variable
     });
+  }
 
-    console.log('this is the token: ', token);
-
+  // get OpenMotics sensor information
+  async getOMSensorInfo(token, sensorId) {
     // fetch data using a get path
     // installation-id -> 215, sensor-id -> 0
-    await fetch('/proxy/api/v1/base/installations/215/sensors/0', {
+    await fetch(`/proxy/api/v1/base/installations/${this.state.installationId}/sensors/${sensorId}`, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -57,28 +154,11 @@ class App extends React.Component {
       console.log(json)
       console.log({'sensor humidity': json.data.status.humidity}, {'sensor temperature': json.data.status.temperature});
       console.log(this.state);
-      const { sensor1 } = this.state
-      sensor1.humidity = json.data.status.humidity
-      sensor1.temperature = json.data.status.temperature
-      this.setState({ sensor1 });
+      const { airAvgs } = this.state
+      airAvgs.humidity = json.data.status.humidity
+      airAvgs.temperature = json.data.status.temperature
+      this.setState({ airAvgs });
       console.log('state set: ', this.state);
-    });
-
-    // fetch outputs
-    fetch('/proxy/api/v1/base/installations/215/outputs', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      // fs.writeFile('./data.json', json.data, (err) => {
-      //   if (err) throw err;
-      //   console.log('file saved!');
-      // })
     });
   }
 
@@ -87,8 +167,8 @@ class App extends React.Component {
       <div className="App">
         <h1>Ceres Green Application:</h1>
           <div id="dash">
-            <div id="box1">Humidity: {this.state.sensor1.humidity}</div>
-            <div id="box2">temperature: {this.state.sensor1.temperature}</div>
+            <div id="box1">Humidity: {this.state.airAvgs.humidity}</div>
+            <div id="box2">temperature: {this.state.airAvgs.temperature}</div>
             <div id="box3">Box 3</div>
             <div id="box4">Box 4</div>
             <div id="box5">Box 5</div>
