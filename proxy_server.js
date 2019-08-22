22f61f0cb6f48d899f745cd48ed5b6b2cb085762
Nickolas.Castle <ncastle@uvm.edu>
2019-08-22 12:04:16 -0400

    changes to deploy to heroku

--- proxy_server.js
+++ proxy_server.js
@@ -3,15 +3,19 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
 const morgan = require('morgan');
-const config = require('./src/config');
+// const config = require('./src/config');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // test comment
 app.use(morgan('dev'));
-app.use(express.static('build'));
-app.use('/proxy', proxy(config.baseurl));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/proxy', proxy(process.env.baseurl));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
